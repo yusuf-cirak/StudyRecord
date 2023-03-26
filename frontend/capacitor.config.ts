6e4bd95d1e6f983e18552740cd56b44caface77a
Yusuf Çırak <yusuf10901@hotmail.com>
2023-03-26 22:00:31 +0300

    Capacitor for converting to mobile

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.studyrecord.app',
+  appName: 'Study Record',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
