bb4388a316b595d00a19fb6baddaae7a500f6f4d
Yusuf Çırak <yusuf10901@hotmail.com>
2023-03-31 22:45:25 +0300

    API sample endpoints, .env variables and constants

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use dotenv::dotenv;
+
+mod constants;
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn get_db_url() -> impl Responder {
+    let database_url =env::var(constants::DATABASE_URL).expect("This env variable does not exist");
+    HttpResponse::Ok().body(database_url)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/db", web::get().to(get_db_url))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
